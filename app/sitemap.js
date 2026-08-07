2b70c0b81180d0865f10c00177a92fed0d34ec31
Manuel Coral <luismanuelcoral000@gmail.com>
2026-08-07 22:15:07 +0200

    Agregar sitemap para Google

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://pasa-por-amarte.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://pasa-por-amarte.vercel.app/nuestra-esencia",
+      lastModified: new Date(),
+    },
+  ];
+}
